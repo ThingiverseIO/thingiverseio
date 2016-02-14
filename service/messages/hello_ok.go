05a7aebfd375a389052acd32d38417e3d2f4d5b1
Joern Weissenborn <joern.weissenborn@gmail.com>
2016-02-14 23:48:37 +0100

    fixed hello_ok.go

--- service/messages/hello_ok.go
+++ service/messages/hello_ok.go
@@ -12,13 +12,8 @@ type HelloOk struct {
 func (*HelloOk) GetType() MessageType { return HELLO_OK }
 
 func (h *HelloOk) Unflatten(d []string) {
-	dec := msgpack.NewDecoder(strings.NewReader(d[0]))
-	dec.Decode(&h)
 }
 
 func (h *HelloOk) Flatten() [][]byte {
-	var payload bytes.Buffer
-	enc := msgpack.NewEncoder(&payload)
-	enc.Encode(h)
-	return [][]byte{payload.Bytes()}
+	return [][]byte{}
 }
