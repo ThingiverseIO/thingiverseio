c597bb2baa6d1973a002ddbd16027397435415c5
joernweissenborn <joern.weissenborn@gmail.com>
2016-03-06 18:39:55 +0100

    added extern definitions

--- shared_library/thingiverseio.c
+++ shared_library/thingiverseio.c
@@ -1,5 +1,47 @@
 #include "tvio.h"
 
+extern int new_input(char* p0);
+
+extern int start_listen(int p0, char* p1);
+
+extern int stop_listen(int p0, char* p1);
+
+extern int call(int p0, char* p1, void* p2, int p3, char** p4, int* p5);
+
+extern int call_all(int p0, char* p1, void* p2, int p3, char** p4, int* p5);
+
+extern int trigger(int p0, char* p1, void* p2, int p3);
+
+extern int trigger_all(int p0, char* p1, void* p2, int p3);
+
+extern int result_ready(int p0, char* p1, int* p2);
+
+extern int retrieve_result_params(int p0, char* p1, void** p2, int* p3);
+
+extern int listen_result_available(int p0, int* p1);
+
+extern int retrieve_listen_result_id(int p0, char** p1, int* p2);
+
+extern int retrieve_listen_result_function(int p0, char** p1, int* p2);
+
+extern int retrieve_listen_result_request_params(int p0, void** p1, int* p2);
+
+extern int retrieve_listen_result_params(int p0, void** p1, int* p2);
+
+extern int retrieve_next_call_all_result_params(int p0, char* p1, void** p2, int* p3);
+
+extern int new_output(char* p0);
+
+extern int get_next_request_id(int p0, char** p1, int* p2);
+
+extern int retrieve_request_function(int p0, char* p1, char** p2, int* p3);
+
+extern int retrieve_request_params(int p0, char* p1, void** p2, int* p3);
+
+extern int reply(int p0, char* p1, void* p2, int p3);
+
+extern int emit(int p0, char* p1, void* p2, int p3, void* p4, int p5);
+
 int tvio_new_input(char* descriptor){
 	return new_input(descriptor);
 }
--- shared_library/thingiverseio.h
+++ shared_library/thingiverseio.h
@@ -3,47 +3,47 @@ extern "C" {
 #endif
 
 
-extern int tvio_new_input(char* descriptor);
+int tvio_new_input(char* descriptor);
 
-extern int tvio_start_listen(int input, char* function);
+int tvio_start_listen(int input, char* function);
 
-extern int tvio_stop_listen(int input, char* function);
+int tvio_stop_listen(int input, char* function);
 
-extern int tvio_call(int input, char* function, void* params, int params_size, char** id, int* id_size);
+int tvio_call(int input, char* function, void* params, int params_size, char** id, int* id_size);
 
-extern int tvio_call_all(int input, char* function, void* params, int params_size, char** id, int* id_size);
+int tvio_call_all(int input, char* function, void* params, int params_size, char** id, int* id_size);
 
-extern int tvio_trigger(int input, char* function, void* params, int params_size);
+int tvio_trigger(int input, char* function, void* params, int params_size);
 
-extern int tvio_trigger_all(int input, char* function, void* params, int params_size);
+int tvio_trigger_all(int input, char* function, void* params, int params_size);
 
-extern int tvio_result_ready(int input, char* id, int* ready);
+int tvio_result_ready(int input, char* id, int* ready);
 
-extern int tvio_retrieve_result_params(int input, char* id, void** params, int* params_size);
+int tvio_retrieve_result_params(int input, char* id, void** params, int* params_size);
 
-extern int tvio_listen_result_available(int input, int* is);
+int tvio_listen_result_available(int input, int* is);
 
-extern int tvio_retrieve_listen_result_id(int input, char** id, int* id_size);
+int tvio_retrieve_listen_result_id(int input, char** id, int* id_size);
 
-extern int tvio_retrieve_listen_result_function(int input, char** function, int* function_size);
+int tvio_retrieve_listen_result_function(int input, char** function, int* function_size);
 
-extern int tvio_retrieve_listen_result_request_params(int input, void** params, int* params_size);
+int tvio_retrieve_listen_result_request_params(int input, void** params, int* params_size);
 
-extern int tvio_retrieve_listen_result_params(int input, void** params, int* params_size);
+int tvio_retrieve_listen_result_params(int input, void** params, int* params_size);
 
-extern int tvio_retrieve_next_call_all_result_params(int input, char* id, void** params, int* params_size);
+int tvio_retrieve_next_call_all_result_params(int input, char* id, void** params, int* params_size);
 
-extern int tvio_new_output(char* descriptor);
+int tvio_new_output(char* descriptor);
 
-extern int tvio_get_next_request_id(int output, char** id, int* id_size);
+int tvio_get_next_request_id(int output, char** id, int* id_size);
 
-extern int tvio_retrieve_request_function(int output, char* id, char** function, int* function_size);
+int tvio_retrieve_request_function(int output, char* id, char** function, int* function_size);
 
-extern int tvio_retrieve_request_params(int output, char* id, void** params, int* params_size);
+int tvio_retrieve_request_params(int output, char* id, void** params, int* params_size);
 
-extern int tvio_reply(int output, char* id, void* params, int params_size);
+int tvio_reply(int output, char* id, void* params, int params_size);
 
-extern int tvio_emit(int output, char* function, void* in_params, int in_params_size, void* params, int params_size);
+int tvio_emit(int output, char* function, void* in_params, int in_params_size, void* params, int params_size);
 
 #ifdef __cplusplus
 }
